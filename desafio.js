6635f683a71753a5db89eab2bb748a32009077d2
Felipe-CEO <felipemelo007silva@gmail.com>
2025-02-05 05:13:19 -0300

    Desafio do Felipe dio

---
+++ desafio.js
@@ -0,0 +1,30 @@
+let heroi
+let xp
+let nivel
+
+heroi = "PAN"
+xp = 95000
+
+
+
+// escrevendo a estrutura de decisão para exibir o nivel do heroi
+if (xp <= 1000){
+    nivel = "Ferro"
+}else if (xp > 1000 && xp <= 2000){
+    nivel  = "Bronze"
+}
+else if (xp > 2000 && xp <= 5000){
+    nivel = "Prata"
+}else if(xp > 5000 && xp <= 7000){
+    nivel = "Ouro"
+}else if (xp > 7000 && xp <= 8000){
+    nivel = "Platina"
+}else if (xp > 8000 && xp <= 9000){
+    nivel = "Ascendente"
+}else if (xp > 9000 && xp <=10000){
+    nivel = "Imortal"
+}else if (xp > 10000){
+    nivel = "Radiante"
+}
+
+console.log("O Heroi : "+ heroi + " estar no nivel : " +  nivel)
